6752e880ebd01213acdb76ecc42e0bc4581ca1cf
Mohamed Sabri Ben Chaabane <sabri@stratosdigital.io>
2025-06-15 01:11:26 +0100

    Dark mode only for admin

--- resources/views/components/layouts/frontend.blade.php
+++ resources/views/components/layouts/frontend.blade.php
@@ -1,14 +1,14 @@
 <x-layouts.base :title="$title ?? null">
-    <div class="dark min-h-screen relative isolate bg-zinc-900 text-white">
+    <div class="dark min-h-screen relative isolate bg-zinc-950 text-white">
         <div class="absolute inset-x-0 -top-40 -z-10 transform-gpu blur-3xl sm:-top-80" aria-hidden="true">
             <div
                 id="background-gradient"
-                class="relative left-[calc(50%-11rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-zinc-800 to-zinc-900 opacity-30 sm:left-[calc(50%-30rem)] sm:w-[72.1875rem]"
+                class="relative left-[calc(50%-11rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-zinc-900 to-zinc-950 opacity-30 sm:left-[calc(50%-30rem)] sm:w-[72.1875rem]"
                 style="clip-path: polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)">
             </div>
         </div>
         <!-- Header -->
-        <header id="main-header" class="fixed top-0 left-0 right-0 z-50 transition-all duration-300 ease-in-out bg-zinc-900/80 backdrop-blur-sm border-b border-zinc-800" style="transform: translateY(-100%);">
+        <header id="main-header" class="fixed top-0 left-0 right-0 z-50 transition-all duration-300 ease-in-out bg-zinc-950/80 backdrop-blur-sm border-b border-zinc-900" style="transform: translateY(-100%);">
             <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
                 <div class="flex items-center justify-between h-16 md:h-20">
                     <div class="flex-shrink-0">
